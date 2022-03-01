f01dbf72ce35f774f3ee56d6166b952c9f728aa2
Ayush <tiwariayush447@gmail.com>
2022-03-02 01:39:28 +0530

    initial commit

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+// import {AppRoutingModule} from './app-routing.module';
+import {MatListModule} from '@angular/material/list';
+
+const MaterialComponents = [
+  MatButtonModule,
+  MatIconModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  
+  MatListModule
+];
+
+
+
+@NgModule({
+  
+  imports: [MaterialComponents],
+  exports: [MaterialComponents]
+})
+export class MaterialModule { }
