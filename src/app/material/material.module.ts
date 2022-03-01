643e916b96c24b416fedc1bf178430cd4ef66da0
Ayush <tiwariayush447@gmail.com>
2022-03-02 01:51:21 +0530

    sidenav

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,7 +3,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
-// import {AppRoutingModule} from './app-routing.module';
+//import {AppRoutingModule} from './app-routing.module';
 import {MatListModule} from '@angular/material/list';
 
 const MaterialComponents = [
